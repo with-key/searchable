1964a5fa0ad03e1960c725fb1ffc5e80a739ca33
with-key <rem9238@kakao.com>
2024-01-24 00:15:24 +0900

    기능 추가 - 컴포넌트 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,62 @@
-import { LiHTMLAttributes, useEffect, useId, useRef, useState } from "react";
-import "./item.css";
+import { useEffect, useRef, useState } from "react";
+import { Combobox } from "./components/Combobox";
 
-interface ItemProps extends LiHTMLAttributes<HTMLLIElement> {}
-function Item(props: ItemProps) {
-  const id = useId();
-  return (
-    <li {...props} id={`option${id}`} role="option" className="SelectItem">
-      옵션 {id}
-    </li>
-  );
-}
+const options = ["사과", "감자", "딸기", "배", "감", "블루베리", "돼지감자"];
 
 function App() {
   const [open, setOpen] = useState(false);
+
   const [cursorIndex, setCursorIndex] = useState(0);
+
   const listRef = useRef<HTMLUListElement | null>(null);
   const inputRef = useRef<HTMLInputElement | null>(null);
 
-  const [value, setValue] = useState("");
-
+  const [inputValue, setInputValue] = useState("");
   const activedescendantId = useRef<string>();
 
-  const handleKeydown: React.KeyboardEventHandler<HTMLInputElement> = (
+  const handleKeydownInput: React.KeyboardEventHandler<HTMLInputElement> = (
     event
   ) => {
+    if (!open) {
+      if (event.key === "ArrowDown") {
+        return setOpen(true);
+      }
+    }
+
     if (event.key === "ArrowDown") {
       event.preventDefault();
 
+      if (listRef.current) {
+        if (cursorIndex + 1 === listRef.current.childElementCount) {
+          return setCursorIndex(0);
+        }
+      }
       setCursorIndex((prev) => prev + 1);
     } else if (event.key === "ArrowUp") {
       event.preventDefault();
+
+      if (listRef.current) {
+        if (cursorIndex === 0) {
+          return setCursorIndex(listRef.current.childElementCount - 1);
+        }
+      }
       setCursorIndex((prev) => prev - 1);
     } else if (event.key === "Enter") {
       if (listRef.current) {
         const arr = [...listRef.current.children];
-        const text =
-          arr[cursorIndex % listRef.current.childElementCount].textContent;
 
-        setValue(text || "");
+        // listRef.current.childElementCount = item의 갯수
+
+        const text = arr[cursorIndex].textContent;
+        console.log(text);
+
+        // console.log("cursorIndex", cursorIndex);
+        // console.log("text", text);
+
+        if (text) {
+          setInputValue(text); // 선택
+          setOpen(false); // 선택 후 닫기
+        }
       }
     }
   };
@@ -49,17 +68,21 @@ function App() {
     setOpen((prev) => !prev);
   };
 
+  const filterList = (options: string[]) => {
+    return options.filter((option) => option.includes(inputValue));
+  };
+
   useEffect(() => {
     if (listRef.current) {
       const childArr = [...listRef.current.children];
       childArr.map((child, index, me) => {
         if (cursorIndex === -1) {
-          child.className = `li data-state=none`;
+          // child.className = `data-state=none`;
         } else if (index === cursorIndex % me.length) {
-          child.className = `li data-state=selected`;
+          // child.className = `data-state=selected`;
           activedescendantId.current = child.id;
         } else {
-          child.className = `li data-state=none`;
+          // child.className = `data-state=none`;
         }
       });
     }
@@ -70,15 +93,23 @@ function App() {
       <input
         ref={inputRef}
         type="text"
-        onKeyDown={handleKeydown}
+        onKeyDown={handleKeydownInput}
+        onFocus={(e) => {
+          setOpen(true);
+        }}
         role="combobox"
         aria-activedescendant={activedescendantId.current || ""}
-        value={value}
+        value={inputValue}
         onChange={(event) => {
           const { value } = event.target;
-          setValue(value);
+          setInputValue(value);
+
+          if (!open) {
+            setOpen(true);
+          }
         }}
       />
+
       <button
         onClick={(e) => {
           handleClickButton(e);
@@ -87,10 +118,17 @@ function App() {
       >
         open
       </button>
+
       {open ? (
         <ul ref={listRef}>
-          {[1, 2, 3, 4].map((option) => {
-            return <Item key={option} />;
+          {filterList(options).map((el, index, me) => {
+            return (
+              <Combobox.Item
+                key={el}
+                value={el}
+                isSelected={index === cursorIndex % me.length}
+              />
+            );
           })}
         </ul>
       ) : null}
--- src/components/Combobox.tsx
+++ src/components/Combobox.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import styles from "./combobox.module.css";
+
 /**
  * context
  */
@@ -20,4 +22,35 @@ export const Root = ({ children }: ComboboxProps) => {
   );
 };
 
-export const Combobox = { Root };
+/**
+ * Item
+ */
+interface ItemProps extends React.LiHTMLAttributes<HTMLLIElement> {
+  isSelected: boolean;
+  value: string;
+}
+
+export function Item({ isSelected, value, ...props }: ItemProps) {
+  const id = React.useId();
+
+  return (
+    <li
+      {...props}
+      id={`option${id}`}
+      role="option"
+      className={styles.item}
+      data-state={isSelected ? "selected" : "none"}
+    >
+      {value}
+    </li>
+  );
+}
+
+/**
+ * Input
+ */
+export function Input() {
+  return <></>;
+}
+
+export const Combobox = { Root, Item };
